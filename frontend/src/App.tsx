2e3cde61f69beb1bc6ff256d859886852dd450e5
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2025-05-05 13:02:39 +0100

    refactor: adjust map container size

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function App() {
   return (
-    <MapContainer
-      center={[39.74362, -8.80705]}
-      zoom={7}
-      className="map-container"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <div id="map-container">
+      <MapContainer center={[39.74362, -8.80705]} zoom={7}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
   );
 }
 
